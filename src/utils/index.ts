68a717a419d96f2cb8b252c008cd3fe67be805f4
quyenptt03 <quyenptt03@gmail.com>
2023-12-12 15:56:44 +0700

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,11 @@
 import { createJWT, isTokenValid, attachCookiesToResponse } from './jwt';
 import createUserToken from './createUserToken';
-import checkPermission from './checkPermission';
+// import checkPermission from './checkPermission';
 
 export {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createUserToken,
-  checkPermission,
+  // checkPermission,
 };
